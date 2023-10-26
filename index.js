48bb76a5a7b970a19c811bcc6c59a1c8b2d15498
RodryTala01 <rodrytalarico@gmail.com>
2023-10-25 22:57:50 -0300

    productos terminado

--- index.js
+++ index.js
@@ -62,77 +62,98 @@ function validar() {
 }
 
 
-var cardGrande = document.getElementById("cardGrande");
-var cardCerda = document.getElementById("cardCerda");
-var cardPita = document.getElementById("cardPita");
-var cardPVC = document.getElementById("cardPVC");
-var cardNylon = document.getElementById("cardNylon");
-var cardAcero = document.getElementById("cardAcero");
-var cardArrastre = document.getElementById("cardArrastre");
-
-var closeButton = document.getElementById("closeButton");
-var titulo = document.getElementById("tituloCardGrande");
-var imagen = document.getElementById("imagenCardGrande").querySelector("img");
-var descripcion = document.getElementById("descripcionCardGrande");
-var precio = document.getElementById("precio");
-
-cardCerda.addEventListener("click", function (event) {
-  cardGrande.style.display = "flex";
-
-  titulo.innerHTML = "CERDA"
-  imagen.src = "imagenes/imetro-cerda.png"
-  descripcion.innerHTML = "Modo lustrado, al igual que el cepillo de cerda, puede utilizar al cepillo de pita para lustrar pisos, tanto de madera, como marmol, terrazo, baldosas, etc. "
-  precio.innerHTML = "Precio: "
-});
-
-cardPita.addEventListener("click", function (event) {
-  cardGrande.style.display = "flex";
-
-  titulo.innerHTML = "PITA"
-  imagen.src = "imagenes/imetro-pita.png"
-  descripcion.innerHTML = "Utilice el cepillo en pisos de madera, para obtener un lustrado superior. En pisos duros, como marmol, terrazo, baldosas, obtendra un resultado excelente, sus pisos quedarán espectaculares con un resplandor soñado. Modo lavado suave, para aquellos pisos que deben ser tratados cuidadosamente, éste tipo de material permite tratarlo convenientemente sin poner en riesgo la integridad de la superficie. Recomendado para pisos con pintura epoxy, porcelanatos, ceramicas, plastificados, entre otros."
-  precio.innerHTML = "Precio: "
-});
-
-cardPVC.addEventListener("click", function (event) {
-  cardGrande.style.display = "flex";
-
-  titulo.innerHTML = "PVC"
-  imagen.src = "imagenes/imetro-ppl.png"
-  descripcion.innerHTML = "El lavado de pisos requiere de la combinación de una herramienta mecanica como el cepillo y un producto químico acorde al tipo de suciedad, en este caso el cepillo de uso generico permite la remoción de suciedad sobre la mayoría de tipos de pisos. Utilizar agua caliente generalmente facilita la limpieza."
-  precio.innerHTML = "Precio: "
-});
-
-cardNylon.addEventListener("click", function (event) {
-  cardGrande.style.display = "flex";
-
-  titulo.innerHTML = "NYLON"
-  imagen.src = "imagenes/imetro-nylon.png"
-  descripcion.innerHTML = "El nylon, material constitutivo, permite una efectiva friccion sobre la alfombra y resulta de suma importancia complementar el uso con un shampoo para alfombras que genere espuma. Para la recollección de la espuma y suciedad removida por el cepillo, recomendamos utilizar una aspiradora polvo/agua para tal efecto. Higienice y limpie convenientemente el cepillo al finalizar el trabajo, para restablecer el formato de los filamentos, debe anjuagar con agua caliente."
-  precio.innerHTML = "Precio: "
-});
-
-cardAcero.addEventListener("click", function (event) {
-  cardGrande.style.display = "flex";
-
-  titulo.innerHTML = "ACERO"
-  imagen.src = "imagenes/imetro-acero.png"
-  descripcion.innerHTML = "Para aquellos casos donde el cepillo pvc no fuera suficiente por tratarse de un tipo de suciedad extrema, aqui es donde destaca el cepillo de acero. Intenso, agresivo, de acción profunda. Ideal combinar con removedores quimicos para complementar la acción de limpieza de la suciedad evitando la fricción innecesaria prolongada de la superficie."
-  precio.innerHTML = "Precio: "
-});
-
-cardArrastre.addEventListener("click", function (event) {
-  cardGrande.style.display = "flex";
-
-  titulo.innerHTML = "DISCO DE ARRASTRE"
-  imagen.src = "imagenes/imetro-arrastre.png"
-  descripcion.innerHTML = "Herramienta indispensable para la utilización de discos de limpieza o 'pads cleaner'. Sus multiples propositos, permiten tambien rasquetear pisos de maderas al poder soportar la viruta."
-  precio.innerHTML = "Precio: "
-});
-
-closeButton.addEventListener("click", function (event) {
-  cardGrande.style.display = "none";
-
-});
 
 
+
+fetch("https://dolarapi.com/v1/dolares/blue")
+  .then(response => response.json())
+  .then(data => {
+    const valorDolarBlue = data.venta;
+    const fecha = new Date(data.fechaActualizacion); 
+    
+    const fechaFormateada = fecha.toLocaleDateString('es-ES', {year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZoneName: 'short' });
+
+  
+    var cardGrande = document.getElementById("cardGrande");
+    var cardCerda = document.getElementById("cardCerda");
+    var cardPita = document.getElementById("cardPita");
+    var cardPVC = document.getElementById("cardPVC");
+    var cardNylon = document.getElementById("cardNylon");
+    var cardAcero = document.getElementById("cardAcero");
+    var cardArrastre = document.getElementById("cardArrastre");
+    
+    var closeButton = document.getElementById("closeButton");
+    var titulo = document.getElementById("tituloCardGrande");
+    var imagen = document.getElementById("imagenCardGrande").querySelector("img");
+    var descripcion = document.getElementById("descripcionCardGrande");
+    var precio = document.getElementById("precio");
+    var actuaizado = document.getElementById("actualizado");
+    
+    cardCerda.addEventListener("click", function (event) {
+      cardGrande.style.display = "flex";
+    
+      titulo.innerHTML = "CERDA"
+      imagen.src = "imagenes/imetro-cerda.png"
+      descripcion.innerHTML = "Modo lustrado, al igual que el cepillo de cerda, puede utilizar al cepillo de pita para lustrar pisos, tanto de madera, como marmol, terrazo, baldosas, etc. "
+      precio.innerHTML = "Precio: $" + (50 * valorDolarBlue).toLocaleString('es-ES');
+      actualizado.innerHTML = "Precio actualizado al " + fechaFormateada;
+    });
+    
+    cardPita.addEventListener("click", function (event) {
+      cardGrande.style.display = "flex";
+    
+      titulo.innerHTML = "PITA"
+      imagen.src = "imagenes/imetro-pita.png"
+      descripcion.innerHTML = "Utilice el cepillo en pisos de madera, para obtener un lustrado superior. En pisos duros, como marmol, terrazo, baldosas, obtendra un resultado excelente, sus pisos quedarán espectaculares con un resplandor soñado. Modo lavado suave, para aquellos pisos que deben ser tratados cuidadosamente, éste tipo de material permite tratarlo convenientemente sin poner en riesgo la integridad de la superficie. Recomendado para pisos con pintura epoxy, porcelanatos, ceramicas, plastificados, entre otros."
+      precio.innerHTML = "Precio: $" + (45 * valorDolarBlue).toLocaleString('es-ES');
+      actualizado.innerHTML = "Precio actualizado al " + fechaFormateada;
+    });
+    
+    cardPVC.addEventListener("click", function (event) {
+      cardGrande.style.display = "flex";
+    
+      titulo.innerHTML = "PVC"
+      imagen.src = "imagenes/imetro-ppl.png"
+      descripcion.innerHTML = "El lavado de pisos requiere de la combinación de una herramienta mecanica como el cepillo y un producto químico acorde al tipo de suciedad, en este caso el cepillo de uso generico permite la remoción de suciedad sobre la mayoría de tipos de pisos. Utilizar agua caliente generalmente facilita la limpieza."
+      precio.innerHTML = "Precio: $" + (42 * valorDolarBlue).toLocaleString('es-ES');
+      actualizado.innerHTML = "Precio actualizado al " + fechaFormateada;
+    });
+    
+    cardNylon.addEventListener("click", function (event) {
+      cardGrande.style.display = "flex";
+    
+      titulo.innerHTML = "NYLON"
+      imagen.src = "imagenes/imetro-nylon.png"
+      descripcion.innerHTML = "El nylon, material constitutivo, permite una efectiva friccion sobre la alfombra y resulta de suma importancia complementar el uso con un shampoo para alfombras que genere espuma. Para la recollección de la espuma y suciedad removida por el cepillo, recomendamos utilizar una aspiradora polvo/agua para tal efecto. Higienice y limpie convenientemente el cepillo al finalizar el trabajo, para restablecer el formato de los filamentos, debe anjuagar con agua caliente."
+      precio.innerHTML = "Precio: $" + (48 * valorDolarBlue).toLocaleString('es-ES');
+      actualizado.innerHTML = "Precio actualizado al " + fechaFormateada;
+    });
+    
+    cardAcero.addEventListener("click", function (event) {
+      cardGrande.style.display = "flex";
+    
+      titulo.innerHTML = "ACERO"
+      imagen.src = "imagenes/imetro-acero.png"
+      descripcion.innerHTML = "Para aquellos casos donde el cepillo pvc no fuera suficiente por tratarse de un tipo de suciedad extrema, aqui es donde destaca el cepillo de acero. Intenso, agresivo, de acción profunda. Ideal combinar con removedores quimicos para complementar la acción de limpieza de la suciedad evitando la fricción innecesaria prolongada de la superficie."
+      precio.innerHTML = "Precio: $" + (60 * valorDolarBlue).toLocaleString('es-ES');
+      actualizado.innerHTML = "Precio actualizado al " + fechaFormateada;
+    });
+    
+    cardArrastre.addEventListener("click", function (event) {
+      cardGrande.style.display = "flex";
+    
+      titulo.innerHTML = "DISCO DE ARRASTRE"
+      imagen.src = "imagenes/imetro-arrastre.png"
+      descripcion.innerHTML = "Herramienta indispensable para la utilización de discos de limpieza o 'pads cleaner'. Sus multiples propositos, permiten tambien rasquetear pisos de maderas al poder soportar la viruta."
+      precio.innerHTML = "Precio: $" + (44 * valorDolarBlue).toLocaleString('es-ES');
+      actualizado.innerHTML = "Precio actualizado al " + fechaFormateada;
+    });
+    
+    closeButton.addEventListener("click", function (event) {
+      cardGrande.style.display = "none";
+    
+    });
+
+
+
+  })
